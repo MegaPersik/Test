64c7cb6c2986a91aeff1b3cf851fc35399587b08
MegaPersik <82091733+MegaPersik@users.noreply.github.com>
2021-04-07 13:52:02 +0300

    Add files via upload

---
+++ SiteCopy/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SiteCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Link = new System.Windows.Forms.TextBox();
+            this.CopyText = new System.Windows.Forms.Button();
+            this.SiteBody = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Link
+            // 
+            this.Link.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Link.Location = new System.Drawing.Point(12, 12);
+            this.Link.Multiline = true;
+            this.Link.Name = "Link";
+            this.Link.Size = new System.Drawing.Size(208, 41);
+            this.Link.TabIndex = 0;
+            this.Link.Text = "https://www.simbirsoft.com/";
+            // 
+            // CopyText
+            // 
+            this.CopyText.Location = new System.Drawing.Point(226, 12);
+            this.CopyText.Name = "CopyText";
+            this.CopyText.Size = new System.Drawing.Size(59, 41);
+            this.CopyText.TabIndex = 2;
+            this.CopyText.Text = "->";
+            this.CopyText.UseVisualStyleBackColor = true;
+            this.CopyText.Click += new System.EventHandler(this.CopyText_Click);
+            // 
+            // SiteBody
+            // 
+            this.SiteBody.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SiteBody.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.SiteBody.Location = new System.Drawing.Point(291, 12);
+            this.SiteBody.Name = "SiteBody";
+            this.SiteBody.ReadOnly = true;
+            this.SiteBody.Size = new System.Drawing.Size(454, 402);
+            this.SiteBody.TabIndex = 3;
+            this.SiteBody.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 419);
+            this.Controls.Add(this.SiteBody);
+            this.Controls.Add(this.CopyText);
+            this.Controls.Add(this.Link);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Link;
+        private System.Windows.Forms.Button CopyText;
+        public System.Windows.Forms.RichTextBox SiteBody;
+    }
+}
+
---
+++ SiteCopy/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+
+namespace SiteCopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CopyText_Click(object sender, EventArgs e)
+        {
+            SiteBody.Text = null;
+            Button_change();
+            SplitText.LoadText(Link.Text);
+
+        }
+        public void Button_change()
+        {
+            CopyText.Enabled = !CopyText.Enabled;
+        }
+    }
+    public class SplitText
+    {
+        static Form1 form = Application.OpenForms[0] as Form1;
+        public static void  LoadText(string link)
+        {
+            try
+            {
+                WebBrowser wb = new WebBrowser();
+                wb.ScriptErrorsSuppressed = true;
+                wb.DocumentCompleted +=
+                    new WebBrowserDocumentCompletedEventHandler(DocumentCompleted);
+                wb.Navigate(link);
+            }
+            catch (Exception msg) 
+            {
+                Log.Write(msg);
+                MessageBox.Show(msg.ToString());
+            }
+        }
+        private static void DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try { 
+            WebBrowser wb = (WebBrowser)sender;
+            wb.Document.ExecCommand("SelectAll", false, null);
+            wb.Document.ExecCommand("Copy", false, null);
+            form.Button_change();
+            wb.Dispose();
+            Paircount();
+            }
+            catch (Exception msg)
+            {
+                Log.Write(msg);
+                MessageBox.Show(msg.ToString());
+            }
+        }
+        private static void Paircount()
+        {
+            try { 
+            var res = Textsplit(Clipboard.GetText().ToUpper());
+            Clipboard.Clear();
+            foreach (var pair in res.OrderByDescending(pair => pair.Value))
+                form.SiteBody.Text +=
+                    (pair.Key + " : " + pair.Value + "\n"); }
+            catch (Exception msg) 
+            {
+                Log.Write(msg);
+                MessageBox.Show(msg.ToString());
+            }
+        }
+        private static Dictionary<string, int> Textsplit(string text)
+        {
+            char[] splitter = new char[] { ' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t' };
+
+            var res = new Dictionary<string, int>();
+
+            foreach (var word in text.Split(splitter).Skip(1))
+            {
+                var count = 0;
+                if (WordCheck(word))
+                {
+                    res.TryGetValue(word, out count);
+                    res[word] = count + 1;
+                }
+            }
+            
+            return res;
+        }
+        private static bool WordCheck(string word)
+        {
+            string[] unicwords = new string[] { "РАЗРАБОТКА", "ПРОГРАММНОГО", "ОБЕСПЕЧЕНИЯ" };
+            foreach (var uword in unicwords)
+            {
+                if (uword == word)
+                    return true;
+            }
+            return false;
+        }
+    }
+    public class Log
+    {
+        private static object sync = new object();
+        public static void Write(Exception ex)
+        {
+            try
+            {
+                string pathToLog = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log");
+                if (!Directory.Exists(pathToLog))
+                    Directory.CreateDirectory(pathToLog);
+                string filename = Path.Combine(pathToLog, string.Format("{0}_{1:dd.MM.yyy}.log",
+                AppDomain.CurrentDomain.FriendlyName, DateTime.Now));
+                string fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] [{1}.{2}()] {3}\r\n",
+                DateTime.Now, ex.TargetSite.DeclaringType, ex.TargetSite.Name, ex.Message);
+                lock (sync)
+                {
+                    File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
